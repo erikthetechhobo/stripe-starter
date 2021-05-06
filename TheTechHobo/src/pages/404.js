49eb17bab4881408ddc1ce7eda1e06dc8737050e
Erik Schroeder <erikthetechhobo@gmail.com>
2021-05-06 12:01:22 -0500

    more cleanup

--- TheTechHobo/src/pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function PageNotFound() {
-    return(
-        <div>you lost</div>
-    );
-}
--- TheTechHobo/src/pages/failed.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Failed() {
-    return(
-        <div>you didn't do it</div>
-    );
-}
